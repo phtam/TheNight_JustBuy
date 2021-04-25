96da7fb334f8f33006237628536a7c6596adb2df
Admin <Admin@DESKTOP-M21QKUC>
2021-04-25 08:53:19 +0700

    Set up Font-end and Database

---
+++ TheNight_JustBuy/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheNight_JustBuy.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Aside()
+        {
+            ViewBag.FullName = "Phạm Hoàng Tâm";
+            ViewBag.Avatar = "";
+            return PartialView();
+        }
+    }
+}
---
+++ TheNight_JustBuy/Common/CommonConstant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheNight_JustBuy.Common
+{
+    public class CommonConstant
+    {
+    }
+}
