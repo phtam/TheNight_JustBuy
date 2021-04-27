c9167cf36d36f48ac3ed8358f0bb1839ebc871eb
Admin <Admin@DESKTOP-M21QKUC>
2021-04-27 18:25:50 +0700

    profile

--- TheNight_JustBuy/Areas/Admin/Models/UserModelForEdit.cs
+++ TheNight_JustBuy/Areas/Admin/Models/UserModelForEdit.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using TheNight_JustBuy.CustomValidation;
 using TheNight_JustBuy.Models;
+using TheNight_JustBuy.ViewModels;
 
 namespace TheNight_JustBuy.Areas.Admin.Models
 {
@@ -15,7 +16,23 @@ namespace TheNight_JustBuy.Areas.Admin.Models
         {
 
         }
-
+        public UserModelForEdit(AdminLoginModel user)
+        {
+            this.UserID = user.UserID;
+            this.Username = user.Username;
+            this.FirstName = user.FirstName;
+            this.LastName = user.LastName;
+            this.Password = user.Password;
+            this.CreditCard = user.CreditCard;
+            this.Gender = user.Gender;
+            this.Birthday = user.Birthday;
+            this.Phone = user.Phone;
+            this.Email = user.Email;
+            this.Avatar = user.Avatar;
+            this.Role = user.Role;
+            this.Status = user.Status;
+            this.Cart = user.Cart;
+        }
         public UserModelForEdit(User user)
         {
             this.UserID = user.UserID;
